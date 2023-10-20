2be909b962e551bfadf049603599b7f19a7753f5
rcfr84 <valcsi20010923@gmail.com>
2023-10-20 12:03:16 +0200

    Alap migrációk elkészítése

--- Projekt/szakdolgozat-app/database/migrations/2023_10_20_092725_create_states_table.php
+++ Projekt/szakdolgozat-app/database/migrations/2023_10_20_092725_create_states_table.php
@@ -11,14 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id('user_id');
+        Schema::create('states', function (Blueprint $table) {
+            $table->id('state_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role')->default('user');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -28,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('states');
     }
 };
--- Projekt/szakdolgozat-app/database/migrations/2023_10_20_093236_create_categories_table.php
+++ Projekt/szakdolgozat-app/database/migrations/2023_10_20_093236_create_categories_table.php
@@ -11,14 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id('user_id');
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id('category_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role')->default('user');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -28,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
