ab2e0958092a65d0448107d8079bc808d3082bea
rcfr84 <valcsi20010923@gmail.com>
2024-02-03 10:35:08 +0100

    Status üzenetek módosítása.
    Status üzenetek módosítása, profil bladek módosítása. A ProfilUpdateRequest megváltoztatása az email cím módosítása miatt.

--- Projekt/szakdolgozat-app/resources/views/profile/partials/update-profile-information-form.blade.php
+++ Projekt/szakdolgozat-app/resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,63 +1,66 @@
 @extends('layouts.app')
+
 @section('update-profile-information-form')
-<section>
-    <header>
-        <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Profil információk') }}
-        </h2>
-    </header>
+    <div class="max-w-2xl mx-auto">
+        <section class="mt-8 space-y-6">
+            <header>
+                <h2 class="text-2xl font-medium text-gray-900">
+                    {{ __('Profil információk') }}
+                </h2>
+            </header>
 
-    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
-        @csrf
-    </form>
+            <form id="send-verification" method="post" action="{{ route('verification.send') }}">
+                @csrf
+            </form>
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
-        @csrf
-        @method('patch')
+            <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+                @csrf
+                @method('patch')
 
-        <div>
-            <x-input-label for="name" :value="__('Név')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
-            <x-input-error class="mt-2" :messages="$errors->get('name')" />
-        </div>
-
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
-            <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
                 <div>
-                    <p class="text-sm mt-2 text-gray-800">
-                        {{ __('Az email címed még nincsen megerősítve.') }}
+                    <x-input-label for="name" :value="__('Név')" />
+                    <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
 
-                        <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            {{ __('Kattintsd ide az újra küldésért.') }}
-                        </button>
-                    </p>
+                <div>
+                    <x-input-label for="email" :value="__('Email')" />
+                    <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
+                    <x-input-error class="mt-2" :messages="$errors->get('email')" />
 
-                    @if (session('status') === 'verification-link-sent')
-                        <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('Az új megerősítő email sikeresen elküldve.') }}
-                        </p>
+                    @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
+                        <div>
+                            <p class="text-sm mt-2 text-gray-800">
+                                {{ __('Az email címed még nincsen megerősítve.') }}
+
+                                <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    {{ __('Kattintsd ide az újra küldésért.') }}
+                                </button>
+                            </p>
+
+                            @if (session('status') === 'Az ellenőrző emailt elküldtük!')
+                                <p class="mt-2 font-medium text-sm text-green-600">
+                                    {{ __('Az új megerősítő email sikeresen elküldve.') }}
+                                </p>
+                            @endif
+                        </div>
                     @endif
                 </div>
-            @endif
-        </div>
 
-        <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Mentés') }}</x-primary-button>
+                <div class="flex items-center gap-4">
+                    <x-primary-button>{{ __('Mentés') }}</x-primary-button>
 
-            @if (session('status') === 'profile-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
-                >{{ __('Mentve.') }}</p>
-            @endif
-        </div>
-    </form>
-</section>
+                    @if (session('status') === 'profile-updated')
+                        <p
+                            x-data="{ show: true }"
+                            x-show="show"
+                            x-transition
+                            x-init="setTimeout(() => show = false, 2000)"
+                            class="text-sm text-gray-600"
+                        >{{ __('Mentve.') }}</p>
+                    @endif
+                </div>
+            </form>
+        </section>
+    </div>
 @endsection
