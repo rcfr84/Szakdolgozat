3d63c014d686584eb729997bd4089f3ed27664a5
rcfr84 <valcsi20010923@gmail.com>
2024-03-07 09:42:21 +0100

    Logó hozzáadása

---
+++ Projekt/szakdolgozat-app/resources/views/components/application-navlogo.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <img src="{{ asset('images/logo2.png') }}" class="w-20 h-10 fill-current text-gray-500" alt="Logo">
+</div>
--- Projekt/szakdolgozat-app/resources/views/layouts/guest.blade.php
+++ Projekt/szakdolgozat-app/resources/views/layouts/guest.blade.php
@@ -23,9 +23,7 @@
         <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js"></script>-->
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100" style="background-image: url('{{ asset('images/background.jpg') }}');">
             <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg" >
