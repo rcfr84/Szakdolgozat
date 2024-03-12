4fcce424d24e10625cddf311d22584a7398c6f9e
rcfr84 <valcsi20010923@gmail.com>
2024-03-12 12:31:00 +0100

    Reszponzívabb nézett kialakítása.
    Tailwind css segítségével.

--- Projekt/szakdolgozat-app/resources/views/layouts/app.blade.php
+++ Projekt/szakdolgozat-app/resources/views/layouts/app.blade.php
@@ -7,13 +7,6 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Bootstrap CSS -->
-        <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">-->
-
-        <!-- Bootstrap JS bundle with Popper -->
-       <!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>-->
-
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -23,12 +16,8 @@
         <script src="{{ asset('js/citySelect.js') }}" defer></script>
         <script src="{{ asset('js/filter.js') }}" defer></script>
 
-        <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css">-->
-
-
     </head>
     <body class="font-sans antialiased">
-        <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js"></script>-->
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
@@ -44,11 +33,11 @@
             <!-- Page Content -->
             <main style="background-image: url('{{ asset('images/background.jpg') }}');">
                 
-                <main class="py-4" style="background-image: url('{{ asset('images/background.jpg') }}');">
+                <main class="py-4 bg-cover bg-center" style="background-image: url('{{ asset('images/background.jpg') }}');">
                     @yield('content')
 
                 </main>
-                <section class="space-y-6">
+                <section class="space-y-6 bg-cover bg-center">
                     <!-- ... (content of the first code section) ... -->
                     @yield('update-profile-information-form')
                     @yield('update-password-form')
