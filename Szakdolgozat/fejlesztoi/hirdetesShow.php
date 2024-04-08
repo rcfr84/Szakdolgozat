0966c6d0b0e5b851954437e2161908a70cb324e9
rcfr84 <valcsi20010923@gmail.com>
2024-04-08 17:45:35 +0200

    Szövegek formázása.
    \emph, a nyilatkozat betöltése, a kódrészletek hosszából töröltem,

--- Szakdolgozat/fejlesztoi/hirdetesShow.php
+++ Szakdolgozat/fejlesztoi/hirdetesShow.php
@@ -1,18 +1,11 @@
-<?php
-
-namespace App\Http\Controllers;
-use App\Models\Advertisement;
-class AdvertisementController extends Controller
+public function show($id)
 {
-    public function show($id)
+    $advertisement = Advertisement::find($id);
+
+    if (!$advertisement) 
     {
-        $advertisement = Advertisement::find($id);
-
-        if (!$advertisement) 
-        {
-            return redirect()->route('advertisements.index')->with('error', 'Nincsen ilyen hirdetés!');
-        }
-
-        return view('advertisements.show', compact('advertisement'));
+        return redirect()->route('advertisements.index')->with('error', 'Nincsen ilyen hirdetés!');
     }
+
+    return view('advertisements.show', compact('advertisement'));
 }
--- Szakdolgozat/fejlesztoi/hirdetesUtvonal.php
+++ Szakdolgozat/fejlesztoi/hirdetesUtvonal.php
@@ -1,8 +1,1 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdvertisementController;
-
 Route::get('/advertisements/{advertisementId}/show', [AdvertisementController::class, 'show'])->name('advertisements.show');
-
-require __DIR__.'/auth.php';
